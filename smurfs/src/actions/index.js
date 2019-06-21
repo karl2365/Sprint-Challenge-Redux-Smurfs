19147d7215be30efec5a818ce7d0f03534e39b04
Karl Beksa <karlbeksa@gmail.com>
2019-06-21 13:17:11 -0400

    functional mvp

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,6 +2,13 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+import axios from 'axios';
+export const FETCHING = 'FETCHING';
+export const SUCCESS = 'SUCCESS';
+export const FAILURE = 'FAILURE';
+export const ADDING = 'ADDING';
+export const ADDING_SUCCESS = 'ADDING_SUCCESS';
+export const ADDING_FAILURE = 'ADDING_FAILLURE';
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +20,25 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const getSmurfs = () => dispatch => {
+  dispatch({ type: FETCHING})
+  axios
+    .get('http://localhost:3333/smurfs')
+    .then(res => {
+      dispatch({ type: SUCCESS, payload: res.data})
+    })
+    .catch(err => {console.log(err)})
+}
+
+export const addSmurf = newSmurf => dispatch => {
+  dispatch({ type: ADDING })
+  axios
+    .post('http://localhost:3333/smurfs', newSmurf)
+    .then(res => {
+      dispatch({type: ADDING_SUCCESS, payload: res.data});
+      console.log(res)
+    })
+    .catch(err => {console.log(err)})
+  
+}
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,18 +1,68 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
+import { FETCHING, SUCCESS, FAILURE, ADDING_SUCCESS, ADDING, ADDING_FAILURE } from '../actions';
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
+//  Your initial/default state for this project could *Although does not have to* look a lot like this
+ 
+const initialState = {
    smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
+   fetching: false,
+   adding: false,
+   updating: false,
+   deleting: false,
+   error: ''
  }
-*/
+
+
+const rootReducer = (state=initialState, action) => {
+  
+
+  switch(action.type){
+
+    case FETCHING:
+      return {
+        ...state,
+        fetching: true,
+        error: ''
+      }
+
+    case SUCCESS:
+        return {
+          ...state,
+          fetching: false,
+          error: '',
+          smurfs: action.payload
+        }
+
+    case FAILURE:
+        return {
+          ...state,
+          error: 'Error fetching smurfs',
+          fetching: false
+        }
+    
+    case ADDING:
+      return {
+        ...state,
+        adding: true,
+        error: ''
+      }
+
+      case ADDING_SUCCESS:
+        return {
+          ...state,
+          adding: false,
+          smurfs: [...state.smurfs, action.payload] 
+        }
+    default: 
+      return{
+        ...state
+      }
+  }
+
+
+}
 
 /*
   You'll only need one smurf reducer for this project.
@@ -21,3 +71,4 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+export default rootReducer;
